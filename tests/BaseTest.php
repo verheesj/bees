b7728d27d21fb3d9ccec43ff15bb1a1a923316eb
Jonathan Verhees <jonathan@verhe.es>
2019-11-19 08:11:49 +0000

    Fixed test

--- tests/BaseTest.php
+++ tests/BaseTest.php
@@ -1,79 +1,97 @@
 <?php
-namespace verheesj\KillTheBeesGame\Tests;
 
-use verheesj\KillTheBeesGame\Base;
+namespace Tests;
+
 use PHPUnit\Framework\TestCase;
+use verheesj\KillTheBeesGame\Base as Game;
 
 class BaseTest extends TestCase
 {
 
-    public function testGetScore()
+    public function testInstantiateObject()
     {
-        $game = new Base;
-        $this->assertIsInt($game->getScore());
-    }
-
-    public function testScore()
-    {
-        $game = new Base;
-
-        for($i = 0; $i < 10; $i++)
-            $game->score();
-
-        $this->assertEquals($game->getScore(), 10);
-    }
-
-    public function testMessage()
-    {
-        $game = new Base;
-        $game->message('This is a test message');
-
-        $this->assertIsBool(in_array("This is a test message", $game->getMessages()));
-    }
-
-    public function testPlaying()
-    {
-        $game = new Base;
-        $game->playing();
-        $this->assertIsBool($game->getGameState() !== Base::STATE_GAMEOVER);
-    }
-
-    public function testGameOver()
-    {
-        $game = new Base;
-        $game->gameOver();
-        $this->assertEquals($game->getGameState(), Base::STATE_GAMEOVER);
-    }
-
-    public function testGetMessages()
-    {
-        $game = new Base;
-        $this->assertIsArray($game->getMessages());
+        $game = new Game();
+        $this->assertIsObject($game);
     }
 
     public function testStart()
     {
-        $game = new Base;
+        $game = new Game();
         $game->start();
-        $this->assertEquals($game->getGameState(), Base::STATE_STARTED);
+
+        $this->assertSame($game->getGameState(), Game::STATE_STARTED);
     }
 
     public function testSetGameState()
     {
-        $game = new Base;
-        $game->setGameState('test');
-        $this->assertEquals($game->getGameState(), 'test');
+        $game = new Game();
+        $game->setGameState('TEST');
+        $this->assertSame($game->getGameState(), 'TEST');
     }
 
     public function testGetGameState()
     {
-        $game = new Base;
-        $this->assertEquals($game->getGameState(), Base::STATE_STARTED);
+        $game = new Game();
+        $this->assertSame($game->getGameState(), Game::STATE_STARTED);
+    }
+
+    public function testScore()
+    {
+        $game = new Game();
+
+        // TODO: What a mess.
+        // 0
+        $game->score(); // + 1
+        $game->score(); // + 1
+        $game->score(); // + 1
+        $game->score(); // + 1
+        $game->score(); // + 1
+        // = 5
+
+        $this->assertIsInt($game->getScore(), 5);
+    }
+
+    public function testGameOver()
+    {
+        $game = new Game();
+        $game->gameOver();
+
+        $this->assertIsBool($game->isGameOver(), true);
+    }
+
+    public function testPlaying()
+    {
+        $game = new Game();
+        $game->playing();
+
+        $this->assertNotEquals($game->getGameState(), Game::STATE_GAMEOVER);
+    }
+
+    public function testGetScore()
+    {
+        $game = new Game();
+
+        $this->assertIsInt($game->getScore());
+    }
+
+    public function testGetMessages()
+    {
+        $game = new Game();
+        $this->assertIsArray($game->getMessages());
+    }
+
+    public function testMessage()
+    {
+        $game = new Game();
+        $game->message('test');
+        $this->assertArrayHasKey(0, $game->getMessages());
     }
 
     public function testIsGameOver()
     {
-        $game = new Base;
-        $this->assertIsBool($game->isGameOver());
+        $game = new Game();
+        $game->setGameState(Game::STATE_GAMEOVER);
+        $this->assertIsBool($game->isGameOver(), true);
     }
+
 }
